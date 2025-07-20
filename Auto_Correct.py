d2fb0f7d86c2e636d4d2402ef6674e0a31fa559b
RahulNataraj-M <rahulnatarajm@gmail.com>
2025-07-20 10:40:03 +0530

    Auto_Correct.py

--- Auto_Correct.py
+++ Auto_Correct.py
@@ -1,12 +1,14 @@
 from spellchecker import SpellChecker
-spell=SpellChecker()
+spell=SpellChecker()  # Create a SpellChecker object to check and correct spelling
 while True:
     text=input("Enter the sentence to be corrected: ")
     words=text.split()
     correct_words=[]
     for word in words:
-        if not spell.unknown([word]):
+        # If the word is correctly spelled, keep it as is
+        if not spell.unknown([word]): 
             correct_words.append(word)
+        # If the word is misspelled, append the most likely correction
         else:
             correct_words.append(spell.correction(word))
     correct_text=' '.join(correct_words)
